d3b8bda13a22facccef9466223fdcad98eccee34
Wanda <doubleuevans@gmail.com>
2019-05-22 13:31:37 -0500

    cache entry and cache put

--- src/cache.c
+++ src/cache.c
@@ -13,6 +13,19 @@ struct cache_entry *alloc_entry(char *path, char *content_type, void *content, i
     // IMPLEMENT ME! //
     ///////////////////
 
+    struct cache_entry *new_cache_entry = malloc(sizeof(struct cache_entry));
+
+    new_cache_entry->path = path;
+    new_cache_entry->content_type = content_type;
+    new_cache_entry->content = content;
+    new_cache_entry->content_length = content_length;
+
+    new_cache_entry->prev = NULL;
+    new_cache_entry->next = NULL;
+
+    return new_cache_entry;
+
+
 }
 
 /**
@@ -129,14 +142,25 @@ void cache_put(struct cache *cache, char *path, char *content_type, void *conten
 //     Algorithm:
 
 //    Allocate a new cache entry with the passed parameters.
+    struct cache_entry *new_cache_entry = alloc_entry(path, content_type, content, content_length);
 //    Insert the entry at the head of the doubly-linked list.
+    dllist_insert_head(cache, new_cache_entry);
 //    Store the entry in the hashtable as well, indexed by the entry's `path`.
+    hashtable_put(cache, path, new_cache_entry);    
 //    Increment the current size of the cache.
+    cache->cur_size += 1;
 //    If the cache size is greater than the max size:
 //      Remove the cache entry at the tail of the linked list.
 //      Remove that same entry from the hashtable, using the entry's `path` and the `hashtable_delete` function.
 //      Free the cache entry.
 //      Ensure the size counter for the number of entries in the cache is correct.
+    if (cache->cur_size > cache->max_size)
+    {
+        struct cache_entry *old_cache_entry = dllist_remove_tail(cache);
+        hashtable_delete(cache->index, old_cache_entry->path);
+        free_entry(old_cache_entry);
+        cache->cur_size -= 1;
+    }    
 
 }
 
